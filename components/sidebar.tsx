964412a2a688eca5cbad420bdf4abe4df7b1e6aa
summer030 <125527872+summer030@users.noreply.github.com>
2024-05-16 22:52:18 +0800

    feat: sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  ClerkLoading,
+  ClerkLoaded,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
 import { cn } from "@/lib/utils";
+
+import { SidebarItem } from "./sidebar-item";
 
 type Props = {
   className?: string;
@@ -12,7 +23,49 @@ export const Sidebar = ({ className }: Props) => {
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src="/mascot.svg"
+            height={40}
+            width={40}
+            alt="Mascot"
+          />
+          <h1 className="text-2xl font-extrabold text-lime-500 tracking-wide">
+            Duolingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col flex-1 gap-y-2">
+        <SidebarItem
+          label="Learn"
+          href="/learn"
+          iconSrc="/learn.svg"
+        />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem
+          label="Quests"
+          href="/quests"
+          iconSrc="/quests.svg"
+        />
+        <SidebarItem
+          label="Shop"
+          href="/shop"
+          iconSrc="/shop.svg"
+        />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
