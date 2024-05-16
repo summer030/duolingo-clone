72b2f82c7892cb1a9d1a27903ac6c377973e01c1
summer030 <karaage4projects@outlook.com>
2024-05-16 22:20:00 +0800

    feat: main layout

---
+++ components/mobile-header.tsx
@@ -0,0 +1,9 @@
+import { MobileSidebar } from "@/components/mobile-sidebar";
+
+export const MobileHeader = () => {
+  return (
+    <nav className="lg:hidden px-6 h-[50px] w-full fixed top-0 flex items-center bg-yellow-300 border-b z-50">
+      <MobileSidebar />
+    </nav>
+  );
+};
