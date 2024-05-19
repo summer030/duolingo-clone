a7b7c7c50ace7a7b103a26bb49f4b00028eb7bb6
summer030 <125527872+summer030@users.noreply.github.com>
2024-05-19 19:09:20 +0800

    feat: user progress

--- db/query.ts
+++
@@ -1,8 +0,0 @@
-import { cache } from "react";
-import db from "@/db/dizzle";
-
-export const getCourses = cache(async () => {
-  const data = await db.query.courses.findMany();
-
-  return data;
-});
