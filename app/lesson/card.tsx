2678e7d11dbef91258cdd0c5f5dc32aa491adcfc
summer030 <125527872+summer030@users.noreply.github.com>
2024-05-29 21:02:27 +0800

    feat: challenge cards

---
+++ app/lesson/card.tsx
@@ -0,0 +1,91 @@
+import Image from "next/image";
+
+import { challenges } from "@/db/schema";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  id: number;
+  imageSrc: string | null;
+  audioSrc: string | null;
+  text: string;
+  shortcut: string;
+  selected?: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+  status?: "correct" | "wrong" | "none";
+  type: (typeof challenges.$inferSelect)["type"];
+};
+
+export const Card = ({
+  id,
+  imageSrc,
+  audioSrc,
+  text,
+  shortcut,
+  selected,
+  onClick,
+  disabled,
+  status,
+  type,
+}: Props) => {
+  return (
+    <div
+      onClick={() => {}}
+      className={cn(
+        "h-full p-4 border-2 rounded-xl border-b-4 hover:bg-black/5 lg:p-6 cursor-pointer active:border-b-2",
+        selected &&
+          "border-sky-300 bg-sky-100 hover:bg-sky-100",
+        selected &&
+          status === "correct" &&
+          "border-lime-300 bg-lime-100 hover:bg-lime-100",
+        selected &&
+          status === "wrong" &&
+          "border-rose-300 bg-rose-100 hover:bg-rose-100",
+        disabled && "point-events-none hover:bg-white",
+        type === "ASSIST" && "lg:p-3 w-full"
+      )}
+    >
+      {imageSrc && (
+        <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
+          <Image src={imageSrc} alt={text} fill />
+        </div>
+      )}
+      <div
+        className={cn(
+          "flex items-center justify-between",
+          type === "ASSIST" && "flex-row-reverse"
+        )}
+      >
+        {type === "ASSIST" && <div />}
+        <p
+          className={cn(
+            "text-neutral-600 text-sm lg:text-base",
+            selected && "text-sky-500",
+            selected &&
+              status === "correct" &&
+              "text-lime-500",
+            selected &&
+              status === "wrong" &&
+              "text-rose-500"
+          )}
+        >
+          {text}
+        </p>
+        <div
+          className={cn(
+            "lg:w-[30px] lg:h-[30px] w-[20px] h-[20px] border-2 flex items-center justify-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold",
+            selected && "border-sky-300 text-sky-500",
+            selected &&
+              status === "correct" &&
+              "border-lime-500 text-lime-500",
+            selected &&
+              status === "wrong" &&
+              "border-rose-500 text-rose-500"
+          )}
+        >
+          {shortcut}
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,14 +5,16 @@ import { useState } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
 
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
 
 type Props = {
   initialLessonId: number;
   initialHearts: number;
   initialPercentage: number;
-  initialLessonChallenges: (typeof challenges.$inferInsert & {
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
     completed: boolean;
-    challengeOptions: (typeof challengeOptions.$inferInsert)[];
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
   })[];
   userSubscription: any; //TODO: Replace with subscription DB type
 };
@@ -28,6 +30,21 @@ export const Quiz = ({
   const [percentage, setPercentage] = useState(
     initialPercentage
   );
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
   return (
     <>
       <Header
@@ -35,6 +52,30 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] lg:px-0 w-full px-6 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble
+                  question={challenge.question}
+                />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status="none"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
