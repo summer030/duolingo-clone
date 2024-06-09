71ffdfd8eb4105f643055e343fc2895105fdfd1b
summer030 <125527872+summer030@users.noreply.github.com>
2024-06-09 15:24:47 +0800

    fix: color bug

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -2,7 +2,7 @@ import { MobileSidebar } from "@/components/mobile-sidebar";
 
 export const MobileHeader = () => {
   return (
-    <nav className="lg:hidden px-6 h-[50px] w-full fixed top-0 flex items-center bg-yellow-300 border-b z-50">
+    <nav className="lg:hidden px-6 h-[50px] w-full fixed top-0 flex items-center bg-lime-500 border-b z-50">
       <MobileSidebar />
     </nav>
   );
