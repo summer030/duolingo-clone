91074be32b90148beef3648441e8e8afbee05625
summer030 <karaage4projects@outlook.com>
2024-06-09 22:25:31 +0800

    feat: challenge practice

---
+++ components/modals/hearts-modal.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { useHeartsModal } from "@/store/use-hearts-modal";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export const HeartsModal = () => {
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = useHeartsModal();
+
+  useEffect(() => setIsClient(true), []);
+
+  const onClick = () => {
+    close();
+    router.push("/store");
+  };
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="w-full flex items-center justify-center mb-5">
+            <Image src="/mascot_bad.svg" alt="Mascot" height={80} width={80} />
+          </div>
+          <DialogTitle className="text-center font-bold text-2xl">
+            You ran out of hearts!
+          </DialogTitle>
+          <DialogDescription className="text-center text-base">
+            Get Pro for unlimited hearts, or purchase them in the store.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="mb-4">
+          <div className="flex flex-col gap-y-4 w-full">
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={onClick}
+            >
+              Get unlimited hearts
+            </Button>
+            <Button
+              variant="primaryOutline"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              No thanks
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
